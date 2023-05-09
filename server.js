d1593276acbf536356e80b9db3f23ef6b9218004
Anita Murali <amurali@unc.edu>
2023-05-09 18:52:54 -0400

    new changes

--- server.js
+++ server.js
@@ -149,6 +149,6 @@ app.get('*', (req, res) => {
     res.status(404).send('404 NOT FOUND')
 })
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server listening on port ${port}`);
+// });
